d2d8a0f101097c41b86feaf54d5864d643091fdb
prnth <pranith_h@yahoo.com.sg>
2025-06-12 11:42:34 +0530

    build error

--- src/app/sketches/instancing/instancedMesh2/InstancedMesh2.tsx
+++ src/app/sketches/instancing/instancedMesh2/InstancedMesh2.tsx
@@ -44,7 +44,6 @@ const InstanceMeshTest = () => {
     }, []);
 
     return (
-        // @ts-expect-error InstancedMesh2 is not a valid JSX element
         <instancedMesh2
             ref={ref}
             args={[geometry, material]}
