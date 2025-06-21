700b56c882e957c3fefe23115aaf7cdf8005644c
prnth <pranith_h@yahoo.com.sg>
2025-06-21 16:00:38 +0530

    faster

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,5 @@
 "use client";
 
-import { Physics, RigidBody } from "@react-three/rapier";
 import { Box, Html, OrbitControls } from "@react-three/drei";
 import { GameCanvas } from "@/shared/GameCanvas";
 import AnimatedModel from "@/shared/HumanoidModel";
@@ -13,28 +12,26 @@ export default function Home() {
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
         <div className="absolute top-0 left-0 w-screen h-screen">
           <GameCanvas camera={{ fov: 75, near: 0.1, far: 1000, position: [0, 1, 2], rotation: [-0.2, 0, 0] }} shadows>
-            <Physics>
-              <AnimatedModel
-                basePath={"/models/human/onimilio/"}
-                model={'rigged.glb'}
-                rotation={[-Math.PI / 2, 0, 0]}
-                height={2}
-                scale={2}
-                position={[0, -0.46, 0]}
-                animationOverrides={{
-                  idle: 'anim/hang.fbx',
-                }}
-              />
-              <ambientLight intensity={0.8} />
-              <Box position={[0, 1.5, 0.24]} args={[0.7, 0.09, 0.05]} castShadow receiveShadow />
-              <Html transform center scale={0.01} position={[0, 1.5, 0.24]} className="text-center">
-                <div className="w-[285px] backdrop-blur-sm scale-[10]">
-                  hi im prnth, nothing to see here
-                </div>
-              </Html>
-              <ShadowLight offset={[-2, -6, -2]} />
-              {/* <OrbitControls /> */}
-            </Physics>
+            <AnimatedModel
+              basePath={"/models/human/onimilio/"}
+              model={'rigged.glb'}
+              rotation={[-Math.PI / 2, 0, 0]}
+              height={2}
+              scale={2}
+              position={[0, -0.46, 0]}
+              animationOverrides={{
+                idle: 'anim/hang.fbx',
+              }}
+            />
+            <ambientLight intensity={0.8} />
+            <Box position={[0, 1.5, 0.24]} args={[0.7, 0.09, 0.05]} castShadow receiveShadow />
+            <Html transform center scale={0.01} position={[0, 1.5, 0.24]} className="text-center">
+              <div className="w-[285px] backdrop-blur-sm scale-[10]">
+                hi im prnth, nothing to see here
+              </div>
+            </Html>
+            <ShadowLight offset={[-2, -6, -2]} />
+            {/* <OrbitControls /> */}
           </GameCanvas>
         </div>
       </main>
