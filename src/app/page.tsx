f8b090b5a3a0a81b33a9469c13e57e8c029f6495
prnth <pranith_h@yahoo.com.sg>
2025-06-25 08:20:42 +0530

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 "use client";
 
-import { Box, Html, OrbitControls, Text } from "@react-three/drei";
+import { Box, Effects, Html, OrbitControls, SpotLight, Text } from "@react-three/drei";
 import { GameCanvas } from "@/shared/GameCanvas";
 import AnimatedModel from "@/shared/HumanoidModel";
 import { ShadowLight } from "./sketches/lighting/shadowmap/ShadowLight";
+import { EffectComposer, Pixelation, Scanline } from "@react-three/postprocessing";
+import Fire from "./sketches/shaders/fire/FireMaterial";
 
 
 export default function Home() {
@@ -12,6 +14,7 @@ export default function Home() {
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
         <div className="absolute top-0 left-0 w-screen h-screen">
           <GameCanvas camera={{ fov: 75, near: 0.1, far: 1000, position: [0, 1, 2], rotation: [-0.2, 0, 0] }} shadows>
+            <color attach="background" args={['darkred']} />
             <AnimatedModel
               basePath={"/models/human/onimilio/"}
               model={'rigged.glb'}
@@ -28,7 +31,12 @@ export default function Home() {
             <Text color="black" textAlign="center" scale={0.04} position={[0, 1.5, 0.27]}>
               hi im prnth, nothing to see here
             </Text>
-            <ShadowLight offset={[-2, -6, -2]} />
+            <pointLight position={[0, -1, 2]} intensity={50} />
+            {/* <ShadowLight offset={[-2, -6, -2]} /> */}
+            <Fire />
+            <EffectComposer>
+              <Scanline density={2} opacity={0.1} />
+            </EffectComposer>
             {/* <OrbitControls /> */}
           </GameCanvas>
         </div>
