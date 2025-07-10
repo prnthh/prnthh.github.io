7bf0637929c7dd83ecdeb9027ca745f2178fb850
prnth <pranith_h@yahoo.com.sg>
2025-07-10 18:31:13 +0530

    ignore server

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-};
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      // Ignore the inner server project in client-side builds
+      config.watchOptions = {
+        ignored: ['**/server/**'],
+      };
+    }
+    return config;
+  },};
 
 export default nextConfig;
