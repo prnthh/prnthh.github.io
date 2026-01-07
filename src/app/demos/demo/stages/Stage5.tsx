03a62f78d1cb21734a9dd5582d31f2aaeac8e1c3
prnth <pranith_h@yahoo.com.sg>
2026-01-07 12:00:09 +0530

    ga

---
+++ src/app/demos/demo/stages/Stage5.tsx
@@ -0,0 +1,4 @@
+// for (float i, z, d; i++ < 55.; o += vec4(9, 4, 2, 1) / d) {vec3 p = z * normalize(FC.rgb * 2. - r.xyy); for (d = 0.; d++ < 9.; p += sin(ceil(p / 9.).zxy + t)); z += d = .1 * abs(p.z + 40.); } o = tanh(o / 6e3);
+
+// for(float i,z,d;i++<55.;o+=vec4(9,4,2,1)/d){vec3 p=z*normalize(FC.rgb*2.-r.xyy);for(d=0.;d++<9.;p+=sin(ceil(p/9.).zxy+t));z+=d=.1*abs(p.z+40.);}o=tanh(o/6e3);
+
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata, Viewport } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Nav from "./Nav";
+import { GoogleAnalytics } from '@next/third-parties/google';
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -39,6 +40,7 @@ export default function RootLayout({
         {children}
         <Nav />
       </body>
+      <GoogleAnalytics gaId="G-ZPS50VC97K" />
     </html>
   );
 }
